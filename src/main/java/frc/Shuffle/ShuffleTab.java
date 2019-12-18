a9a26e20841e2c7d0303cbf215913132179fa3de
nicholas blackburn <nickblackburn02@gmail.com>
2019-12-18 15:55:39 -0500

    finished shuffletab class

--- src/main/java/frc/Shuffle/ShuffleTab.java
+++ src/main/java/frc/Shuffle/ShuffleTab.java
@@ -6,11 +6,16 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 public class ShuffleTab {
     public String TabName;
 
-    // Shuffle_tab.CreateTab(namer)
     public void createTab(ShuffleboardTab tab) {
 
         // Creates a Shuffleboard tab named from String Defined Tabname
         tab = Shuffleboard.getTab(TabName);
+    }
+
+    public void selectTab(Shuffleboard shuffleboard) {
+
+        // Selects user made tab in shuffleboard
+        Shuffleboard.selectTab(TabName);
     }
 
 }
